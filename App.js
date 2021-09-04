f47d58be22be666703a78590ac93d42288920159
Akash Rana <56543037+AKASHRANA931@users.noreply.github.com>
2021-09-05 01:28:41 +0530

    add

--- App.js
+++ App.js
@@ -23,3 +23,36 @@ class App extends React.Component{
 }
 
 export default App;
+
+// Form Get Value With the help of functional component
+
+import {useState} from 'react'
+import './App.css'
+
+function App(){
+  const [Name,setName] = useState("")
+  const [City , setCity] = useState("")
+  const [check, setCheck] = useState(false)
+
+  function getForm(e){
+    e.preventDefault();
+    console.log(Name,City,check)
+  }
+  return(
+    <div className="App">
+    <form onSubmit={getForm}>
+    <h1>Forms get data</h1>
+      <input type="text" placeholder="Enter your name" onChange={(e)=>setName(e.target.value)}/><br/><br/>
+      <select onChange={(e)=>setCity(e.target.value)}>
+        <option>Choose City</option>
+        <option>Saharanpur</option>
+        <option>Muzaffarnagar</option>
+        <option>Meerut</option>
+      </select><br/><br/>
+      <input type="checkbox" onChange={(e)=>setCheck(e.target.checked)}/><span>Accept terms and Conditions</span><br/><br/>
+      <button type="submit">Submit</button>
+    </form>
+    </div>
+  );
+}
+export default App
